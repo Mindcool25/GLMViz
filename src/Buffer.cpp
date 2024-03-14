336ab3ba93eea6c66d2c9809eb118d1d5a2861a4
mindcool24 <thezachm24@gmail.com>
2024-03-13 18:34:11 -0600

    Fixed build errors, format buffer and gl_utils

--- src/Buffer.cpp
+++ src/Buffer.cpp
@@ -18,139 +18,140 @@
  */
 
 #include "Buffer.hpp"
-#include <type_traits>
 #include <algorithm>
+#include <array>
+#include <type_traits>
 
-template<typename T>
-Buffer<T>::Buffer(const size_t size){
-	static_assert(std::is_arithmetic<T>::value, "Buffer<T> only supports arithmetic types!");
+template <typename T> Buffer<T>::Buffer(const size_t size) {
+  static_assert(std::is_arithmetic<T>::value,
+                "Buffer<T> only supports arithmetic types!");
 
-	v_buffer.resize(size);
-	this->size = size;
-	new_data = true;
+  v_buffer.resize(size);
+  this->size = size;
+  new_data = true;
 }
 
-template<typename T>
-std::unique_lock<std::mutex> Buffer<T>::lock(){
-	return std::unique_lock<std::mutex>(m);
+template <typename T> std::unique_lock<std::mutex> Buffer<T>::lock() {
+  return std::unique_lock<std::mutex>(m);
 }
 
 // ceiling division
-template<typename T>
-static inline T ceil_div(const T x, const T y){
-	return x/y + (x % y !=0);
+template <typename T> static inline T ceil_div(const T x, const T y) {
+  return x / y + (x % y != 0);
 }
 
-
-template<typename T>
-inline void Buffer<T>::i_write(T buf[], const size_t n){
-	// move old data
-	std::copy(v_buffer.begin() + n, v_buffer.end(), v_buffer.begin());
-	// append new data
-	std::copy(buf, buf + n, v_buffer.end() - n);
+template <typename T> inline void Buffer<T>::i_write(T buf[], const size_t n) {
+  // move old data
+  std::copy(v_buffer.begin() + n, v_buffer.end(), v_buffer.begin());
+  // append new data
+  std::copy(buf, buf + n, v_buffer.end() - n);
 }
 
-template<typename T>
-inline void Buffer<T>::i_write(const std::vector<T>& buf, const size_t n){
-	// move old data
-	std::copy(v_buffer.begin() + n, v_buffer.end(), v_buffer.begin());
-	// append new data
-	std::copy(buf.begin(), buf.begin() + n, v_buffer.end() - n);
+template <typename T>
+inline void Buffer<T>::i_write(const std::vector<T> &buf, const size_t n) {
+  // move old data
+  std::copy(v_buffer.begin() + n, v_buffer.end(), v_buffer.begin());
+  // append new data
+  std::copy(buf.begin(), buf.begin() + n, v_buffer.end() - n);
 }
 
-template<typename T>
-void Buffer<T>::write(T buf[], const size_t n){
-	auto lock = this->lock();
-	new_data = true;
+template <typename T> void Buffer<T>::write(T buf[], const size_t n) {
+  auto lock = this->lock();
+  new_data = true;
 
-	// limit data to write
-	size_t length = std::min(n, size);
-	i_write(buf, length);
+  // limit data to write
+  size_t length = std::min(n, size);
+  i_write(buf, length);
 }
 
-template<typename T>
-void Buffer<T>::write(const std::vector<T>& buf){
-	auto lock = this->lock();
-	new_data = true;
+template <typename T> void Buffer<T>::write(const std::vector<T> &buf) {
+  auto lock = this->lock();
+  new_data = true;
 
-	// limit data to write
-	size_t length = std::min(buf.size(), size);
-	i_write(buf, length);
+  // limit data to write
+  size_t length = std::min(buf.size(), size);
+  i_write(buf, length);
 }
 
-template<typename T>
-void Buffer<T>::write_offset(T buf[], const size_t n, const size_t gap, const size_t offset){
-	auto lock = this->lock();
-	new_data = true;
+template <typename T>
+void Buffer<T>::write_offset(T buf[], const size_t n, const size_t gap,
+                             const size_t offset) {
+  auto lock = this->lock();
+  new_data = true;
 
-	// limit data to write
-	size_t length = std::min(ceil_div(n - offset, gap), size);
-	size_t current = offset;
+  // limit data to write
+  size_t length = std::min(ceil_div(n - offset, gap), size);
+  size_t current = offset;
 
-	// resize intermediate buffer
-	if(ibuf.size() < length) ibuf.resize(length);
-	for(size_t i = 0; i<length; i++){
-		ibuf[i] = buf[current];
-		current += gap;
-	}
-	i_write(ibuf, length);
+  // resize intermediate buffer
+  if (ibuf.size() < length)
+    ibuf.resize(length);
+  for (size_t i = 0; i < length; i++) {
+    ibuf[i] = buf[current];
+    current += gap;
+  }
+  i_write(ibuf, length);
 }
 
-template<typename T>
-void Buffer<T>::write_offset(const std::vector<T>& buf, const size_t gap, const size_t offset){
-	auto lock = this->lock();
-	new_data = true;
+template <typename T>
+void Buffer<T>::write_offset(const std::vector<T> &buf, const size_t gap,
+                             const size_t offset) {
+  auto lock = this->lock();
+  new_data = true;
 
-	// limit data to write
-	size_t length = std::min(ceil_div(buf.size() - offset, gap), size);
-	size_t current = offset;
+  // limit data to write
+  size_t length = std::min(ceil_div(buf.size() - offset, gap), size);
+  size_t current = offset;
 
-	// resize intermediate buffer
-	if(ibuf.size() < length) ibuf.resize(length);
-	for(size_t i = 0; i<length; i++){
-		ibuf[i] = buf[current];
-		current += gap;
-	}
-	i_write(ibuf, length);
+  // resize intermediate buffer
+  if (ibuf.size() < length)
+    ibuf.resize(length);
+  for (size_t i = 0; i < length; i++) {
+    ibuf[i] = buf[current];
+    current += gap;
+  }
+  i_write(ibuf, length);
 }
 
-template<typename T>
-void Buffer<T>::resize(const size_t n){
-	auto lock = this->lock();
-	if(size != n){
-		size = n;
-		v_buffer.resize(n);
-		new_data = true;
-	}
+template <typename T> void Buffer<T>::resize(const size_t n) {
+  auto lock = this->lock();
+  if (size != n) {
+    size = n;
+    v_buffer.resize(n);
+    new_data = true;
+  }
 }
 
 // calculate the rms value of all the audio data in the buffer
-template <typename T>
-float Buffer<T>::rms(){
-	auto lock = this->lock();
-	float rms = 0;
-	unsigned i = 0;
+template <typename T> float Buffer<T>::rms() {
+  auto lock = this->lock();
+  float rms = 0;
+  unsigned i = 0;
 
-	constexpr unsigned N = 4;
-	// temp sum vector
-	alignas(16) std::array<float,N> vrms;
-	for(unsigned k=0; k<N; k++) vrms[k] = 0;
+  constexpr size_t N = 4;
+  // temp sum vector
+  alignas(16) std::array<float, N> vrms;
+  for (size_t k = 0; k < N; k++)
+    vrms[k] = 0;
 
-	for(; i < size/N; i++){
-		// load 4 integers and convert them to float
-		alignas(16) std::array<float, N> vdata;
-		for(unsigned k=0; k<N; k++) vdata[k] = (float)v_buffer[i*N + k];
+  for (; i < size / N; i++) {
+    // load 4 integers and convert them to float
+    alignas(16) std::array<float, N> vdata;
+    for (size_t k = 0; k < N; k++)
+      vdata[k] = (float)v_buffer[i * N + k];
 
-		#pragma omp simd
-		for(unsigned k=0; k<N; k++) vrms[k] += vdata[k] * vdata[k];
-	}
-	// calculate sum
-	for(unsigned k=0; k<N; k++) rms += vrms[k];
+#pragma omp simd
+    for (size_t k = 0; k < N; k++)
+      vrms[k] += vdata[k] * vdata[k];
+  }
+  // calculate sum
+  for (size_t k = 0; k < N; k++)
+    rms += vrms[k];
 
-	// calculate remaining values
-	i *= N;
-	for(; i < size; i++){
-		 rms += v_buffer[i] * v_buffer[i];
-	}
-	return rms;
+  // calculate remaining values
+  i *= N;
+  for (; i < size; i++) {
+    rms += v_buffer[i] * v_buffer[i];
+  }
+  return rms;
 }
--- src/GL_utils.hpp
+++ src/GL_utils.hpp
@@ -7,12 +7,13 @@
 #include <GL/glew.h>
 #endif
 
+#include <cstddef>
 #include <vector>
 
 /*!
-	\file
+        \file
 
-	OpenGL RAII wrappers and convenience functions.
+        OpenGL RAII wrappers and convenience functions.
 */
 
 #define GLDEBUG_S(x) #x
@@ -23,231 +24,253 @@
 namespace GL {
 //! Buffer RAII wrapper
 struct Buffer {
-	inline Buffer() noexcept { glGenBuffers(1, &id); };
-	inline ~Buffer() noexcept { glDeleteBuffers(1, &id); };
-	//! move ctor
-	Buffer(Buffer&& b) noexcept: id(b.id) { b.id = 0; };
+  inline Buffer() noexcept { glGenBuffers(1, &id); };
+  inline ~Buffer() noexcept { glDeleteBuffers(1, &id); };
+  //! move ctor
+  Buffer(Buffer &&b) noexcept : id(b.id) { b.id = 0; };
 
-	Buffer(const Buffer&) = delete; //!< disable copying
-	Buffer& operator=(Buffer&&) = default; //!< move assignment
+  Buffer(const Buffer &) = delete;        //!< disable copying
+  Buffer &operator=(Buffer &&) = default; //!< move assignment
 
-	/*!
-		Binds the buffer to the GL_ARRAY_BUFFER target.
-	*/
-	inline void bind() const noexcept { glBindBuffer(GL_ARRAY_BUFFER, id); };
+  /*!
+          Binds the buffer to the GL_ARRAY_BUFFER target.
+  */
+  inline void bind() const noexcept { glBindBuffer(GL_ARRAY_BUFFER, id); };
 
-	inline void tfbind() { glBindBufferBase(GL_TRANSFORM_FEEDBACK_BUFFER, 0, id); };
+  inline void tfbind() {
+    glBindBufferBase(GL_TRANSFORM_FEEDBACK_BUFFER, 0, id);
+  };
 
-	/*!
-		Binds the buffer to the GL_ARRAY_BUFFER target.
-	*/
-	inline void operator()() const noexcept { bind(); };
+  /*!
+          Binds the buffer to the GL_ARRAY_BUFFER target.
+  */
+  inline void operator()() const noexcept { bind(); };
 
-	/*!
-		Binds the buffer to the specified target.
-		\param target the target to bind to
-	*/
-	inline void bind(GLenum target) const noexcept { glBindBuffer(target, id); };
-	/*!
-		Binds the buffer to the specified target.
-		\param target the target to bind to
-	*/
-	inline void operator()(GLenum target) const noexcept { bind(target); };
+  /*!
+          Binds the buffer to the specified target.
+          \param target the target to bind to
+  */
+  inline void bind(GLenum target) const noexcept { glBindBuffer(target, id); };
+  /*!
+          Binds the buffer to the specified target.
+          \param target the target to bind to
+  */
+  inline void operator()(GLenum target) const noexcept { bind(target); };
 
-	/*!
-		Binds the default buffer to the GL_ARRAY_BUFFER target.
-	*/
-	static inline void unbind() noexcept { glBindBuffer(GL_ARRAY_BUFFER, 0); };
+  /*!
+          Binds the default buffer to the GL_ARRAY_BUFFER target.
+  */
+  static inline void unbind() noexcept { glBindBuffer(GL_ARRAY_BUFFER, 0); };
 
-	/*!
-		Binds the default buffer to the specified target.
-		\param target the target to bind to
-	*/
-	static inline void unbind(GLenum target) noexcept { glBindBuffer(target, 0); };
+  /*!
+          Binds the default buffer to the specified target.
+          \param target the target to bind to
+  */
+  static inline void unbind(GLenum target) noexcept {
+    glBindBuffer(target, 0);
+  };
 
-	GLuint id; //!< buffer handle
+  GLuint id; //!< buffer handle
 };
 
 //! VAO RAII wrapper
 struct VAO {
-	inline VAO() noexcept { glGenVertexArrays(1, &id); };
-	inline ~VAO() noexcept { glDeleteVertexArrays(1, &id); };
-	//! move ctor
-	VAO(VAO&& v) noexcept: id(v.id) { v.id = 0; };
+  inline VAO() noexcept { glGenVertexArrays(1, &id); };
+  inline ~VAO() noexcept { glDeleteVertexArrays(1, &id); };
+  //! move ctor
+  VAO(VAO &&v) noexcept : id(v.id) { v.id = 0; };
 
-	VAO(const VAO&) = delete; //!< disable copying
-	VAO& operator=(VAO&&) = default; //!< move assignment
+  VAO(const VAO &) = delete;        //!< disable copying
+  VAO &operator=(VAO &&) = default; //!< move assignment
 
-	inline void bind() const noexcept { glBindVertexArray(id); };
-	//! bind VAO
-	inline void operator()() const noexcept { bind(); };
+  inline void bind() const noexcept { glBindVertexArray(id); };
+  //! bind VAO
+  inline void operator()() const noexcept { bind(); };
 
-	//! unbind VAO
-	static inline void unbind() noexcept { glBindVertexArray(0); };
+  //! unbind VAO
+  static inline void unbind() noexcept { glBindVertexArray(0); };
 
-	GLuint id; //!< VAO handle
+  GLuint id; //!< VAO handle
 };
 
 //! Shader RAII wrapper
 struct Shader {
 public:
-	Shader(const char* code, GLuint type);
-	inline ~Shader() noexcept { glDeleteShader(id); };
-	//! move ctor
-	Shader(Shader&& s) noexcept: id(s.id) { s.id = 0; };
+  Shader(const char *code, GLuint type);
+  inline ~Shader() noexcept { glDeleteShader(id); };
+  //! move ctor
+  Shader(Shader &&s) noexcept : id(s.id) { s.id = 0; };
 
-	Shader(const Shader&) = delete; //!< disable copying
-	Shader& operator=(Shader&&) = default; //!< move assignment
+  Shader(const Shader &) = delete;        //!< disable copying
+  Shader &operator=(Shader &&) = default; //!< move assignment
 
-	GLuint id; //!< handle
+  GLuint id; //!< handle
 };
 
 //! Shader Program RAII wrapper
 class Program {
 public:
-	Program();
-	inline ~Program() noexcept { glDeleteProgram(id); };
-	//! move ctor
-	Program(Program&& p): id(p.id) { p.id = 0; };
+  Program();
+  inline ~Program() noexcept { glDeleteProgram(id); };
+  //! move ctor
+  Program(Program &&p) : id(p.id) { p.id = 0; };
 
-	Program(const Program&) = delete; //!< disable copying
-	Program& operator=(Program&&) = default; //!< move assignment
+  Program(const Program &) = delete;        //!< disable copying
+  Program &operator=(Program &&) = default; //!< move assignment
 
-	/*!
-		Link the specified shaders to the Program.
-		\param shs shaders to link
-	*/
-	template<typename ... T> void link(T& ... shs) {
-		attach(shs...);
-		glLinkProgram(id);
-		detach(shs...);
-		check_link_status();
-	}
+  /*!
+          Link the specified shaders to the Program.
+          \param shs shaders to link
+  */
+  template <typename... T> void link(T &...shs) {
+    attach(shs...);
+    glLinkProgram(id);
+    detach(shs...);
+    check_link_status();
+  }
 
-	template<typename ... T> void link_TF(const size_t n, const char** varyings, T&... shs){
-		glTransformFeedbackVaryings(id, n, varyings, GL_INTERLEAVED_ATTRIBS);
-		link(shs...);
-	}
+  template <typename... T>
+  void link_TF(const size_t n, const char **varyings, T &...shs) {
+    glTransformFeedbackVaryings(id, n, varyings, GL_INTERLEAVED_ATTRIBS);
+    link(shs...);
+  }
 
-	/*!
-		Link the specified shaders to the Program.
-		\param shs shaders to link
-	*/
-	void link_vector(const std::vector<Shader>& shs) {
-		for(auto& sh : shs) {
-			attach(sh);
-		}
+  /*!
+          Link the specified shaders to the Program.
+          \param shs shaders to link
+  */
+  void link_vector(const std::vector<Shader> &shs) {
+    for (auto &sh : shs) {
+      attach(sh);
+    }
 
-		glLinkProgram(id);
+    glLinkProgram(id);
 
-		for(auto& sh : shs) {
-			detach(sh);
-		}
+    for (auto &sh : shs) {
+      detach(sh);
+    }
 
-		check_link_status();
-	}
+    check_link_status();
+  }
 
-	/*!
-		Check if the Program is linked correctly. Throws std::invalid_argument on failure.
-	*/
-	void check_link_status();
+  /*!
+          Check if the Program is linked correctly. Throws std::invalid_argument
+     on failure.
+  */
+  void check_link_status();
 
-	/*!
-		Attach multiple shaders to program.
-		\param shs shaders to attach
-	*/
-	template<typename ... T> inline void attach(const Shader& sh, T& ... shs) { attach(sh); attach(shs...); };
-	/*!
-		Attach specified shader to program.
-		\param sh shader to attach
-	*/
-	inline void attach(const Shader& sh) { glAttachShader(id, sh.id); };
+  /*!
+          Attach multiple shaders to program.
+          \param shs shaders to attach
+  */
+  template <typename... T> inline void attach(const Shader &sh, T &...shs) {
+    attach(sh);
+    attach(shs...);
+  };
+  /*!
+          Attach specified shader to program.
+          \param sh shader to attach
+  */
+  inline void attach(const Shader &sh) { glAttachShader(id, sh.id); };
 
-	/*!
-		Detach multiple shaders from the program.
-		\param shs shaders to detach
-	*/
-	template<typename ... T> inline void detach(const Shader& sh, T& ... shs) { detach(sh); detach(shs...); };
-	/*!
-		Detach specified shader from the program.
-		\param sh shader to detach
-	*/
-	inline void detach(const Shader& sh) { glDetachShader(id, sh.id); };
+  /*!
+          Detach multiple shaders from the program.
+          \param shs shaders to detach
+  */
+  template <typename... T> inline void detach(const Shader &sh, T &...shs) {
+    detach(sh);
+    detach(shs...);
+  };
+  /*!
+          Detach specified shader from the program.
+          \param sh shader to detach
+  */
+  inline void detach(const Shader &sh) { glDetachShader(id, sh.id); };
 
-	//! use shader
-	inline void use() const noexcept { glUseProgram(id); };
-	//! use shader
-	inline void operator()() const noexcept { use(); };
+  //! use shader
+  inline void use() const noexcept { glUseProgram(id); };
+  //! use shader
+  inline void operator()() const noexcept { use(); };
 
-	/*!
-		Get program id.
-		\return Program handle
-	*/
-	inline GLuint get_id() const noexcept { return id; };
+  /*!
+          Get program id.
+          \return Program handle
+  */
+  inline GLuint get_id() const noexcept { return id; };
 
-	/*!
-		Get the uniform location of the specified uniform name.
-		\param name Uniform name
-		\return Uniform location
-	*/
-	inline GLint get_uniform(const char* name) const { return glGetUniformLocation(id, name); };
-	/*!
-		Get the attribute location of the specified attribute name.
-		\param name Attribute name
-		\return Attribute location
-	*/
-	inline GLint get_attrib(const char* name) const { return glGetAttribLocation(id, name); };
+  /*!
+          Get the uniform location of the specified uniform name.
+          \param name Uniform name
+          \return Uniform location
+  */
+  inline GLint get_uniform(const char *name) const {
+    return glGetUniformLocation(id, name);
+  };
+  /*!
+          Get the attribute location of the specified attribute name.
+          \param name Attribute name
+          \return Attribute location
+  */
+  inline GLint get_attrib(const char *name) const {
+    return glGetAttribLocation(id, name);
+  };
 
 private:
-	GLuint id; //!< Program handle
+  GLuint id; //!< Program handle
 };
 
 //! Texture RAII wrapper
 struct Texture {
-	inline Texture() noexcept { glGenTextures(1, &id); };
-	inline ~Texture() noexcept { glDeleteTextures(1, &id); };
-	//! move ctor
-	Texture(Texture&& t) noexcept: id(t.id) { t.id = 0; };
+  inline Texture() noexcept { glGenTextures(1, &id); };
+  inline ~Texture() noexcept { glDeleteTextures(1, &id); };
+  //! move ctor
+  Texture(Texture &&t) noexcept : id(t.id) { t.id = 0; };
 
-	Texture(const Texture&) = delete; //!< disable copying
-	Texture& operator=(Texture&&) = default; //!< move assignment
+  Texture(const Texture &) = delete;        //!< disable copying
+  Texture &operator=(Texture &&) = default; //!< move assignment
 
-	inline void bind(GLenum target) const noexcept { glBindTexture(target, id); };
-	inline void operator()(GLenum target) const noexcept { bind(target); };
+  inline void bind(GLenum target) const noexcept { glBindTexture(target, id); };
+  inline void operator()(GLenum target) const noexcept { bind(target); };
 
-	static inline void unbind(GLenum target) noexcept { glBindTexture(target, 0); };
+  static inline void unbind(GLenum target) noexcept {
+    glBindTexture(target, 0);
+  };
 
-	GLuint id; //!< handle
+  GLuint id; //!< handle
 };
 
 //! Framebuffer RAII wrapper
 struct FBO {
-	inline FBO() noexcept { glGenFramebuffers(1, &id); };
-	inline ~FBO() noexcept { glDeleteFramebuffers(1, &id); };
-	//! move ctor
-	FBO(FBO&& t) noexcept: id(t.id) { t.id = 0; };
+  inline FBO() noexcept { glGenFramebuffers(1, &id); };
+  inline ~FBO() noexcept { glDeleteFramebuffers(1, &id); };
+  //! move ctor
+  FBO(FBO &&t) noexcept : id(t.id) { t.id = 0; };
 
-	FBO(const FBO&) = delete; //!< disable copying
-	FBO& operator=(FBO&&) = default; //!< move assignment
+  FBO(const FBO &) = delete;        //!< disable copying
+  FBO &operator=(FBO &&) = default; //!< move assignment
 
-	inline void bind() const noexcept { bind(GL_FRAMEBUFFER); };
-	inline void operator()() const noexcept { bind(); };
+  inline void bind() const noexcept { bind(GL_FRAMEBUFFER); };
+  inline void operator()() const noexcept { bind(); };
 
-	inline void bind(GLenum target) const noexcept { glBindFramebuffer(target, id); };
-	inline void operator()(GLenum target) const noexcept { bind(target); };
+  inline void bind(GLenum target) const noexcept {
+    glBindFramebuffer(target, id);
+  };
+  inline void operator()(GLenum target) const noexcept { bind(target); };
 
-	static inline void unbind() noexcept { unbind(GL_FRAMEBUFFER); };
-	static inline void unbind(GLenum target) noexcept { glBindFramebuffer(target, 0); };
+  static inline void unbind() noexcept { unbind(GL_FRAMEBUFFER); };
+  static inline void unbind(GLenum target) noexcept {
+    glBindFramebuffer(target, 0);
+  };
 
-	GLuint id; //!< handle
+  GLuint id; //!< handle
 };
 
 /*!
-	Print error message if an opengl error occured.
-	\param str error message that gets printed
+        Print error message if an opengl error occured.
+        \param str error message that gets printed
 */
-void get_error(const char*, const char* function = nullptr);
+void get_error(const char *, const char *function = nullptr);
 
 //! Initialize GL context
 void init();
-}
+} // namespace GL
